e13d9a0e03ce897308df35b11390387bc2fba517
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2025-07-22 21:15:43 +0900

    init

---
+++ src/main/java/com/example/betcall/BetCallApplication.java
@@ -0,0 +1,13 @@
+package com.example.betcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetCallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetCallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/betcall/BetCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.betcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetCallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
